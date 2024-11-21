b238b70bab754acd322ed0fb2dbcbbc22800fac8
Miquel <miquelplanapinyol@gmail.com>
2024-11-21 19:56:24 +0100

    tests and add github actions configuration

---
+++ src/test/java/chatsystem_planasleiman/network/UDPTests.java
@@ -0,0 +1,38 @@
+package chatsystem_planasleiman.network;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UDPTests {
+    private static final int test_port = 1871;
+    @Test
+    void sendReceiveTest()throws Exception{
+        List<String> testMessages = Arrays.asList("alice", "bob", "chloe", "multi\nline String", "Éàç");
+
+
+        List<String> receivedMessages = new ArrayList<>();
+        UDPServer server = new UDPServer(test_port);
+            server.addObserver(message -> {
+                System.out.println("Received: " + message.content());
+                receivedMessages.add(message.content());
+            });
+        server.start();
+
+        for (String msg : testMessages){
+            UDPSender.sendLocalhost(test_port, msg);
+        }
+        
+
+        Thread.sleep(100);
+        System.out.println(receivedMessages);
+        assertEquals(testMessages.size(), receivedMessages.size());
+        assertEquals(testMessages,receivedMessages);
+        
+
+    }
+
+}
